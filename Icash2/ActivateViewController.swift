e243e5ae30d5bfae2e06721aef3f54b211f3d77f
BahaeddinSagar <bahaeddinsagar@bahaeddins-mac.local>
2017-03-20 08:07:05 +0200

    before adding crypto

--- Icash2/ActivateViewController.swift
+++ Icash2/ActivateViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class Activate : UIViewController {
+
+class ActivateViewController : UIViewController {
+    
+    @IBOutlet weak var cardNumberTextField: UITextField!
+    @IBOutlet weak var actiCodeTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var swi: UISwitch!
+    var installid : String!
+    var acticode : String!
+    var phone : String!
+    var CardNumber : String!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,17 +36,192 @@ class Activate : UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func switchClicked(_ sender: UISwitch) {
+        if swi.isOn {
+            actiCodeTextField.text=""
+            phoneTextField.text=""
+            actiCodeTextField.isEnabled = false
+            phoneTextField.isEnabled = false
+        } else {
+            actiCodeTextField.isEnabled = true
+            phoneTextField.isEnabled = true
+        }
+        
+    }
     
    
     
     @IBAction func confirm(_ sender: Any) {
-        
+        CardNumber = cardNumberTextField.text!
+        if swi.isOn{
+            acticode = actiCodeTextField.text!
+            phone = phoneTextField.text!
+            
+            if (CardNumber == "" || acticode == "") {
+                _=SweetAlert().showAlert("الرجاء ادخال رقم البطاقة و رقم التفعيل ", subTitle: " " , style: AlertStyle.error)
+                
+            }else if phone.characters.count != 11 {
+                _=SweetAlert().showAlert("الرجاء ادخال رقم الهاتف بصيغة  ", subTitle: "2189XXXXXXXX " , style: AlertStyle.error)
+            }else {
+                var code = CardNumber + phone
+                code = code + acticode
+                let ConfirmationCode = Card.makeHash(str: (code) , level: 7)
+                ActivateReq(CardNo: CardNumber, tel: phone, ActivationCode: acticode, ConfirmationCode: ConfirmationCode)
+            }
+        }else{
+            // TOD read cardNumber field and call ActicateInstallation
+            if CardNumber != "" {
+                ActivateInstallationReq(CardNo: CardNumber)
+            }else{
+                _=SweetAlert().showAlert("الرجاء ادخال رقم البطاقة  ", subTitle: " " , style: AlertStyle.error)
+                
+            }
+        }
         
    
         
-        self.performSegue(withIdentifier: "goToConfirm", sender: self)
         
     }
+    
+    
+    
+    
+    
+    
+    func ActivateReq ( CardNo:  String, tel: String, ActivationCode: String, ConfirmationCode: String){
+        SwiftSpinner.show(" يتم الاتصال بالخادم ...")
+        let requestedurl : URL = URL (string: "http://icashapi.azurewebsites.net/api/ActivateReq/"+"/"+CardNo+"/"+tel+"/"+ActivationCode+"/"+ConfirmationCode)!
+        let urlRequest : URLRequest = URLRequest(url: requestedurl)
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: urlRequest) {
+            (data, response, error) in
+            SwiftSpinner.hide()
+            
+            if response != nil {
+                let httpResponse = response as! HTTPURLResponse
+                if (httpResponse.statusCode == 200){
+                    
+                    let dataString = String(data: data!,encoding: .utf8)
+                    let result = Int(dataString!)
+                    
+                    
+                    switch(result!){
+                    case -8 :
+                        //print("issued but not distributed yet")
+                        OperationQueue.main.addOperation {
+                            _ = SweetAlert().showAlert("خطأ", subTitle: "issued but not distributed yet", style: AlertStyle.error)
+                        }
+                        
+                    case -7 :
+                        //print("internal error")
+                        OperationQueue.main.addOperation {
+                            _ = SweetAlert().showAlert("خطأ", subTitle: "internal error", style: AlertStyle.error)
+                        }
+                        
+                    case -6 :
+                        //to get instulattion ID
+                        self.ActivateInstallationReq(CardNo: CardNo)
+                        
+                        
+                    case -5 :
+                        //print("card doesn't exist")
+                        OperationQueue.main.addOperation {
+                            _ = SweetAlert().showAlert("خطأ", subTitle: "card doesn't exist", style: AlertStyle.error)
+                        }
+                        
+                    case -4 :
+                        print("card already activated")
+                        //to get instulattion ID
+                        self.ActivateInstallationReq(CardNo: CardNo)
+                        
+                        
+                    case -3 :
+                        //print("issued but not distributed yet")
+                        OperationQueue.main.addOperation {
+                            _ = SweetAlert().showAlert("خطأ", subTitle: "issued but not distributed yet", style: AlertStyle.error)
+                        }
+                        
+                    case -2 :
+                        //print("issued but not distributed yet")
+                        OperationQueue.main.addOperation {
+                            _ = SweetAlert().showAlert("خطأ", subTitle: "issued but not distributed yet", style: AlertStyle.error)
+                        }
+                        
+                    case -1 :
+                        print("illigale request")
+                        OperationQueue.main.addOperation {
+                            _ = SweetAlert().showAlert("حطأ", subTitle: "illigale request", style: AlertStyle.error)
+                        }
+                        
+                    case 0..<1000000 :
+                        // save finalresult which is InstallationID
+                        if KeychainWrapper.defaultKeychainWrapper.set(dataString!, forKey: "installID")  {
+                            self.installid = dataString!
+                            // TODO : Show an alert to user to wait for the SMS
+                            //print("All good,message will be sent to you soon")
+                            // to show the result
+                            OperationQueue.main.addOperation {
+                                _ = SweetAlert().showAlert("نجحت العملية",subTitle: "ستصلك رسالة برقم التأكيد قريبا ", style: AlertStyle.success)
+                                self.performSegue(withIdentifier: "goToConfirm", sender: self)
+                            }
+                        }
+                    default :
+                        OperationQueue.main.addOperation {
+                            _ = SweetAlert().showAlert("فشلت العملية",subTitle: "نأمل المحاولة مجددا ", style: AlertStyle.error)
+                        }
+                        break
+                    }
+                }
+            } else {
+                OperationQueue.main.addOperation {
+                    _ = SweetAlert().showAlert("فشلت العملية",subTitle: "نأمل التحقق من الوصول للانترنت ", style: AlertStyle.error)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+    func ActivateInstallationReq(CardNo: String) {
+        SwiftSpinner.show("Connecting to server...")
+        let requestURL : URL = URL(string: "http://icashapi.azurewebsites.net/api/ActivateInstallationReq/"+CardNo)!
+        let urlRequest : URLRequest = URLRequest(url: requestURL)
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: urlRequest) {
+            (data,response,error) in
+            SwiftSpinner.hide()
+            if response != nil {
+                let dataString = String (data:data!,encoding: .utf8)
+                let result = Int(dataString!)
+                if (result! > 0)
+                {
+                    self.installid = dataString!
+                    if KeychainWrapper.defaultKeychainWrapper.set(self.installid, forKey: "installID"){
+                        OperationQueue.main.addOperation {
+                             _ = SweetAlert().showAlert("نجحت العملية",subTitle: "ستصلك رسالة برقم التأكيد قريبا ", style: AlertStyle.success)
+                            self.performSegue(withIdentifier: "goToConfirm", sender: self)
+                        }
+                    }
+                } else {
+                    OperationQueue.main.addOperation {
+                        _ = SweetAlert().showAlert("خطأ", subTitle: "نأمل المحاولة مجددا ", style: AlertStyle.error)
+                        print("operation failed")
+                    }
+                }
+            } else {
+                OperationQueue.main.addOperation {
+                    _ = SweetAlert().showAlert("فشلت العملية",subTitle: "نأمل التحقق من الوصول للانترنت ", style: AlertStyle.error)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
     
     
 }
---
+++ Icash2/CardInfo.swift
@@ -0,0 +1,97 @@
+//
+//  CardInfo.swift
+//  iCash
+//
+//  Created by Bahaeddin Sagar on 8/23/16.
+//  Copyright © 2016 Umbrella. All rights reserved.
+//
+
+import Foundation
+
+
+
+//extenstion for string to transfer from HEXA to DECIMAL
+extension String {
+    var drop0xPrefix:          String { return hasPrefix("0x") ? String(characters.dropFirst(2)) : self }
+    var drop0bPrefix:          String { return hasPrefix("0b") ? String(characters.dropFirst(2)) : self }
+    var hexaToDecimal:            Int { return Int(drop0xPrefix, radix: 16) ?? 0 }
+    var hexaToBinaryString:    String { return String(hexaToDecimal, radix: 2) }
+    var decimalToHexaString:   String { return String(Int(self) ?? 0, radix: 16) }
+    var decimalToBinaryString: String { return String(Int(self) ?? 0, radix: 2) }
+    var binaryToDecimal:          Int { return Int(drop0bPrefix, radix: 2) ?? 0 }
+    var binaryToHexaString:    String { return String(binaryToDecimal, radix: 16) }
+}
+
+class Card {
+    
+    var CardNo:String?
+//    var AccNo:String?
+    var installID:String?
+    var installHashKey:String?
+    var voucherCounter:Int
+    var VHPinCode:String?
+    var VHEPinCode:String?
+    
+    //init to get data from keychain
+    init(){
+       CardNo = KeychainWrapper.defaultKeychainWrapper.string(forKey: "CardNo")!
+//     AccNo = KeychainWrapper.defaultKeychainWrapper().stringForKey("AccNo")!
+       installID = KeychainWrapper.defaultKeychainWrapper.string(forKey: "installID")!
+       installHashKey = KeychainWrapper.defaultKeychainWrapper.string(forKey: "installHashKey")!
+       voucherCounter = KeychainWrapper.defaultKeychainWrapper.integer(forKey: "voucherCounter")!
+       VHPinCode = KeychainWrapper.defaultKeychainWrapper.string(forKey: "HPinCode")!
+       VHEPinCode = KeychainWrapper.defaultKeychainWrapper.string(forKey: "HEPinCode")!
+    }
+    //Special init for start
+    init(startint : Int){
+        voucherCounter=0
+        KeychainWrapper.defaultKeychainWrapper.set(0, forKey: "voucherCounter")
+    }
+    
+    //Save variables
+    func save() ->Bool{
+       let saveSuccessfulCardNo: Bool = KeychainWrapper.defaultKeychainWrapper.set(CardNo!, forKey: "CardNo")
+//        let saveSuccessfulAccNo: Bool = KeychainWrapper.defaultKeychainWrapper().setString(AccNo!, forKey: "AcccNo")
+        let saveSuccessfulInstallID: Bool = KeychainWrapper.defaultKeychainWrapper.set(installID!, forKey: "installID")
+        let saveSuccessfulInstallHash: Bool = KeychainWrapper.defaultKeychainWrapper.set(installHashKey!, forKey: "installHashKey")
+        let saveSuccessfulCounter: Bool = KeychainWrapper.defaultKeychainWrapper.set(voucherCounter, forKey: "voucherCounter")
+        let saveSuccessfulPIN: Bool = KeychainWrapper.defaultKeychainWrapper.set(VHPinCode!, forKey: "HPinCode")
+        let saveSuccessfulEPIN: Bool = KeychainWrapper.defaultKeychainWrapper.set(VHEPinCode!, forKey: "HEPinCode")
+        return(saveSuccessfulCardNo && saveSuccessfulInstallID && saveSuccessfulInstallHash && saveSuccessfulPIN && saveSuccessfulEPIN && saveSuccessfulCounter)
+        
+ 
+    }
+    
+  
+    //HEXA TO DECIMAL
+    static func HexToDeci(_ Hex:String) -> String{
+        
+        let result = Hex.hexaToDecimal
+        print(result)
+        
+        return "\(result)"
+    }
+    //get substring from a string
+    static func getSubString(_ str:String , startindex: Int, endindex: Int) -> String{
+        
+        return str[str.characters.index(str.startIndex, offsetBy: startindex)...str.characters.index(str.startIndex, offsetBy: endindex)]
+    }
+    
+    static func getlastString(_ str:String ,startindex: Int)-> String{
+        var sstartindex = -1 * startindex
+     return  str.substring(from:str.index(str.endIndex, offsetBy: sstartindex))
+    }
+    
+    static func makeHash(str: String, level: Int) -> String{
+        var strr = getlastString(str.md5!, startindex: level+3 )
+        strr = getSubString(strr, startindex: 0, endindex: level-1)
+        return HexToDeci(strr)
+    }
+    
+    static func changeToFloat(_ num: Float) -> String {
+      return String(format: "%.4f", num)
+    }
+        
+    
+    
+}
